d2e922f245d86c2cce6452303762d82067022de2
calummccarthy <calumgmccarthy@gmail.com>
2023-05-15 20:01:50 +0100

    Fixed previous hours feature and added avg feature

--- increment.js
+++ increment.js
@@ -12,15 +12,30 @@ var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
 
 
 function create(time) {
-    var list = date + ' ' + time;
+    var list = date + '  :  ' + time;
     dates.push(list);
-    console.log(dates);
 }
 
 function load() {
     for (let i=0; i < dates.length; i++) {
         let li = document.createElement('li')
-        li.innerHTML = (dates[0]);
+        li.setAttribute("class", "prev_hrs")
+        li.innerHTML = (dates[i]);
         document.body.appendChild(li);
     }
+}
+
+function avg() {
+    let sum = 0;
+    for (let i=0; i < dates.length; i++) {
+        let cur_date = dates[i];
+        let number = parseFloat(cur_date.slice(14, 17));
+        sum += number;
+    }
+
+    average = sum / dates.length;
+
+    let btn1 = document.getElementById("average");
+    btn1.innerHTML = average;
+
 }
