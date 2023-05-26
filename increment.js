1765f293dd01c4252cbe31f46d970104e67b196c
calummccarthy <calumgmccarthy@gmail.com>
2023-05-26 16:05:15 +0100

    Fixed Timer, Added Animations

--- increment.js
+++ increment.js
@@ -50,48 +50,45 @@ let mins = 0;
 let secs = 0;
 
 startBtn.addEventListener("click", () => {
-    if (paused == true) {
+    if(paused){
         paused = false;
         startTime = Date.now() - elapsedTime;
         intervalId = setInterval(updateTime, 1000);
     }
 });
 pauseBtn.addEventListener("click", () => {
-    if (!paused) 
+    if(!paused){
         paused = true;
         elapsedTime = Date.now() - startTime;
         clearInterval(intervalId);
-        startTime = 0;
-        elapsedTime = 0;
-        currentTime = 0;
-        paused = true;
-        intervalId;
-        hrs = 0;
-        mins = 0;
-        secs = 0;
-        timeDisplay.textContent = "00:00:00"
-    
+    }
 });
 resetBtn.addEventListener("click", () => {
     paused = true;
     clearInterval(intervalId);
-
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00";
 });
 
-function updateTime() {
+function updateTime(){
     elapsedTime = Date.now() - startTime;
 
     secs = Math.floor((elapsedTime / 1000) % 60);
-    mins = Math.floor((elapsedTime / 1000 * 60) % 60);
-    hrs = Math.floor((elapsedTime / 1000 * 60 * 60) % 60);
-
-    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
 
     secs = pad(secs);
     mins = pad(mins);
     hrs = pad(hrs);
 
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
     function pad(unit){
-        return (("0") + unit).length > 2 ? unit: "0" + unit
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
     }
 }
